287645f80bc926f15e01158f4dc90c870a25b3be
Staaliii <kevinstaal@gmx.net>
2015-06-16 15:01:18 +0200

    Test
    Beschreibung

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>DevBug</title>
+        <link rel="stylesheet" href="stile/haupt.css" />
+    </head>
+    <body>
+        <img id="rechts" src="bilder/2.png" />
+    </body>
+</html>
